37644b387c91339617405a2f5f1798b877a29079
TrifanBogdan24 <trifanbogdan24@gmail.com>
2025-08-31 01:01:29 +0300

    Move method-2 files into subfolder

---
+++ method-2-folder/README.md
@@ -0,0 +1,305 @@
+# 🏎️ To pit or not pit...
+
+A **data management** CLI app, built in **C**
+to analyze sensors values of a Ferrari **Formula 1** car.
+
+
+> ⚠️ Spoiler Alert!
+> This is **NOT** a **data acquisition** project.
+
+> Sensors values have already been 'collected' and stored in `*.dat` **binary files**.
+
+## Story Telling
+
+In this **fictional scenario in which my project takes place**,
+Ferrari engineers needed my software expertise to stay competitive during F1 season.
+They discovered that faulty sensors (likely cheap ones from AliExpress 😉)
+were feeding incorrect data into their systems, affecting performance decisions.
+
+**My job was clear**:
+develop solutions to detect and eliminate bad data,
+ensuring the team works with accurate insights.
+By doing so, I played a direct role in helping Ferrari chase victory on the F1 track.
+
+## Sensor Types
+
+In this scenario, there are 2 types of faulty sensors in a Formula 1 car:
+1. **PMU** (Power Management Unit) Sensors
+2. **Tire** Sensors
+
+Each sensor type has its priority and associated function callss.
+
+
+
+## 👨‍💻 Software Commands I Provide to Ferrari Engineers
+
+### 1. Simple Print (`print <index>`)
+---
+
+It will the print the data received by the sensor the given index.
+
+In the case of an invalid value, the following error message will appear: `Index not in range!`.
+
+
+Expected output for **PMU Sensors**:
+```
+Power Management Unit
+Voltage: <voltage>
+Current: <current>
+Power Consumption: <power_consumption>
+Energy Regen: <energy_regen>
+Energy Storage: <energy_storage>
+```
+
+Expected output for **Tire Sensors**:
+```
+Tire Sensor
+Pressure: <pressure>
+Temperature: <temperature>
+Wear Level: <wear_level>
+Performance Score: <computed score>/Performance Score: Not Calculated
+```
+
+
+
+### 2. Priority Printing
+---
+
+It will show data from all sensors, sorting after their type:
+**PMU sensors** are more important than **Tire Sensors**.
+
+
+### 3. Analyze (`analyze <index>`)
+---
+
+It will call the functions associated to the given sensor.
+
+Like **simple print**, the `Index not in range!` will appear if the index is invalid.
+
+### 4. `clear`
+---
+
+It will delete mulfunctioning sensors (containing error-prone values) from the array of sensors.
+
+
+### 5. `exit`
+---
+
+Gracefully shut down the program after free-ing the memory.
+
+
+## 👨‍💻 Preview
+
+
+![gif](./preview.gif)
+
+
+## ▶️ How to run the CLI app
+
+
+```sh
+cd src/
+make
+./main ../checker/input/<sensor-file>.dat < ../checker/input/<command-file>.in
+```
+
+Example:
+
+```sh
+./main ../checker/input/sensors_print_easy_1.dat < ../checker/input/commands_print_easy_1.in
+```
+
+🧹 Don't forget to remove the object and binary files:
+```sh
+make clean
+```
+
+
+## `>_` User Input
+
+- **Command line argument**: path to the `*.dat` file containing sensors data
+  - > Use `sensors_*.dat` from [`checker/input/`](./checker/input/)
+- **Stdandard input**: the *"operations"* to perform on sensors data
+  - > Use `commands_*.in` from [`checker/input/`](./checker/input/)
+
+💡 A more elegant solution to run the executable
+would be to use **input redirection** (`<`) to provide commands from a file,
+as shown in the examples above.
+
+
+
+
+## Data Structures for Sensors
+
+
+
+Sensor:
+```c
+typedef struct {
+	enum sensor_type sensor_type;
+	void *sensor_data;
+	int nr_operations;
+	int *operations_idxs;
+} sensor;
+```
+
+
+**PMU** sensor data:
+```c
+typedef struct __attribute__((__packed__)) {
+	float voltage;
+	float current;
+	float power_consumption;
+	int energy_regen;
+	int energy_storage;
+} power_management_unit;
+```
+
+
+**Tire** sensor data:
+```c
+typedef struct __attribute__((__packed__)) {
+	float pressure;
+	float temperature;
+	int wear_level;
+	int performace_score;
+} tire_sensor;
+
+```
+
+
+
+## 👨‍💻 My Implementation
+
+### Sensor Priority
+---
+
+
+Ferrari’s telemetry prioritizes **PMU** readings over **Tire** readings.
+The output array must have **all PMU sensors first**, followed by all Tire sensors.
+
+If we receive the following input (read order):
+```
+Tire_1 Tire_2 PMU_1 PMU_2 Tire_3 PMU_3 Tire_4 Tire_5 PMU_4
+```
+
+The vector will contain the sensors in the following order (sorted by priority):
+```
+PMU_1 PMU_2 PMU_3 PMU_4 Tire_1 Tire_2 Tire_3 Tire_4 Tire_5
+```
+
+### 🔗 Sorting Sensors using **Linked Lists**
+---
+
+
+Sorting the array of sensors by type takes place while reading the input file.
+
+Let me walk you through.
+
+To store sensors and group them by type, I used **two linked lists**
+1. 🔗 one for **PMU** sensors
+2. 🔗 the second one for **Tire** sensors
+
+
+```c
+typedef struct node {
+  sensor sensor;
+  struct node *next;
+} ListNode;
+```
+
+> 🎯 With this **data structure**,
+> I achieved to preserve the original file order for each sensor type.
+
+Keeping a **pointer to the end of each list** (tail pointer)
+**makes append operations faster**,
+in just `O(1)`, without having to iterate the list before adding a new sensor.
+
+
+Let's take a look at **PMU** sensor for example:
+```c
+// Read PMU sensor data from file
+sensor sensor = fread_PMU_sensor_values(fin);
+
+if (!pmu_sensors_head) {
+  // Initialize linked list
+  pmu_sensors_head = pmu_sensors_tail = new_list_node(sensor);
+} else {
+  // Append to the end of list
+  pmu_sensors_tail->next = new_list_node(sensor);
+  pmu_sensors_tail = pmu_sensors_tail->next;
+}
+```
+
+After reading, I **concatenated** both lists **into an array**
+(deallocating memory as I iterate them).
+
+The resulted array will contain all **PMU** sensors first,
+followed by all **Tire** sensors.
+
+Excluding file I/O, sorting runs at a **time complexity** of exact `Θ(N)`.
+
+### Analyze Sensors (Function call)
+---
+
+
+
+**Requirement**: The coresponding sensor functions MUST BE called through the array.
+
+> Under no circumstances the functions can be explicitally called.
+
+
+In order to do so, I've created a **pointer of eight elements**, **each element pointing to a specific function**.
+
+For the actual call of the function,
+I made a **pointer casting** to extract the desired function.
+The call is made using the `sensor_data` field of the `sensor` data structure.
+
+
+```c
+int idx = 0;     // sensor index
+scanf("%d", &idx);
+if (0 <= idx && idx <= num_sensors - 1) {
+  void *operations[NUM_OPERATIONS];
+  get_operations(operations);
+  for (int i = 0; i < sensors_array[idx].nr_operations; i++) {
+    // operations call :
+    ((void (*)()) operations[sensors_array[idx]
+      .operations_idxs[i]]) (sensors_array[idx]
+      .sensor_data);
+      // equivalent to f();
+  }
+}
+```
+
+
+
+
+
+## ✅🔁 Automated Tests
+
+Curious how everything gets tested automatically?
+
+Take a look at the [checker/](./checker/) folder.
+
+
+### 🧪 GitHub Actions | CI Pipeline
+---
+
+No test suite is complete without **Continous Integration**.
+
+I've set up the automated checker with **GitHub Actions**
+to run tests on every push and pull request.
+
+Take a look at the CI workflow here:
+[.github/workflows/CI-checker-tests.yml](.github/workflows/CI-checker-tests.yml).
+
+
+### 🌃 Overnight Testing
+---
+
+The overnight testing is automatically triggered by `method-1` branch.
+
+> ⏱️ ONLY the **default branch** can run scheduled jobs.
+>
+> ⚠️ Therefore, it would be redundant to configure a workflow here.
---
+++ method-2-folder/src/main.c
@@ -0,0 +1,379 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "structs.h"
+
+#define COMMAND_BUFFER_LEGNTH 10
+#define NUM_OPERATIONS        8
+#define FULL_PERCENTAGE       100
+
+#define MIN_TIRE_PRESSURE 19
+#define MAX_TIRE_PRESURE  28
+#define MAX_TIRE_TEMP     120
+
+#define MAX_PMU_POWER_CONSUMPTION 1000
+#define MIN_PMU_CURRENT -100
+#define MAX_PMU_CURRENT 100
+#define MIN_PMU_VOLTAGE 10
+#define MAX_PMU_VOLTAGE 20
+
+#define PRINT   "print"
+#define ANALYSE "analyze"
+#define CLEAR   "clear"
+#define EXIT    "exit"
+
+
+ListNode *new_list_node(sensor sensor)
+{
+	ListNode *node = (ListNode *) malloc(sizeof(ListNode));
+	node->sensor = sensor;
+	node->next = NULL;
+	return node;
+}
+
+/*
+* It will return 0 if sensor values are within optimal range
+* and -1, otherwise.
+*/
+int verify_sensor(sensor senzor)
+{
+	if (senzor.sensor_type == TIRE) {
+		tire_sensor *tire_data = (tire_sensor *)(senzor.sensor_data);
+		if (tire_data->pressure < MIN_TIRE_PRESSURE)
+			return -1;
+		if (tire_data->pressure > MAX_TIRE_PRESURE)
+			return -1;
+		if (tire_data->temperature < 0)
+			return -1;
+		if (tire_data->temperature > MAX_TIRE_TEMP)
+			return -1;
+		if (tire_data->wear_level < 0)
+			return -1;
+		if (tire_data->wear_level > FULL_PERCENTAGE)
+			return -1;
+		return 0;
+	}
+	if (senzor.sensor_type == PMU) {
+		power_management_unit *pmu_data =
+			(power_management_unit *)(senzor.sensor_data);
+		if (pmu_data->voltage < MIN_PMU_VOLTAGE)
+			return -1;
+		if (pmu_data->voltage > MAX_PMU_VOLTAGE)
+			return -1;
+		if (pmu_data->current < MIN_PMU_CURRENT)
+			return -1;
+		if (pmu_data->current > MAX_PMU_CURRENT)
+			return -1;
+		if (pmu_data->power_consumption < 0)
+			return -1;
+		if (pmu_data->power_consumption > MAX_PMU_POWER_CONSUMPTION)
+			return -1;
+		if (pmu_data->energy_regen < 0)
+			return -1;
+		if (pmu_data->energy_regen > FULL_PERCENTAGE)
+			return -1;
+		if (pmu_data->energy_storage < 0)
+			return -1;
+		if (pmu_data->energy_storage > FULL_PERCENTAGE)
+			return -1;
+		return 0;
+	}
+	return -1;
+}
+
+
+/* Print format for PMU sensor :
+	Power Management Unit
+	Voltage: <voltage>
+	Current: <current>
+	Power Consumption: <power_consumption>
+	Energy Regen: <energy_regen>
+	Energy Storage: <energy_storage>
+*/
+void print_PMU_sensor_status(power_management_unit *pmu_data)
+{
+	printf("Power Management Unit\n");
+	printf("Voltage: %.2f\n", pmu_data->voltage);
+	printf("Current: %.2f\n", pmu_data->current);
+	printf("Power Consumption: %.2f\n", pmu_data->power_consumption);
+	printf("Energy Regen: %d%%\n", pmu_data->energy_regen);
+	printf("Energy Storage: %d%%\n", pmu_data->energy_storage);
+}
+
+
+/* Print format for TIRE sensor :
+	Tire Sensor
+	Pressure: <pressure>
+	Temperature: <temperature>
+	Wear Level: <wear_level>
+	Performance Score: <computed score>/Performance Score: Not Calculated
+*/
+void print_TIRE_sensor_status(tire_sensor *tire_data)
+{
+	printf("Tire Sensor\n");
+	printf("Pressure: %.2f\n", tire_data->pressure);
+	printf("Temperature: %.2f\n", tire_data->temperature);
+	printf("Wear Level: %d%%\n", tire_data->wear_level);
+	if (tire_data->performace_score) {
+		printf("Performance Score: %d\n", tire_data->performace_score);
+	} else {
+		printf("Performance Score: Not Calculated\n");
+	}
+}
+
+
+void print_status(sensor senzor)
+{
+	if (senzor.sensor_type == PMU) {
+		print_PMU_sensor_status((power_management_unit *)(senzor.sensor_data));
+	} else if (senzor.sensor_type == TIRE) {
+		print_TIRE_sensor_status((tire_sensor *)(senzor.sensor_data));
+	}
+}
+
+
+
+void fread_sensor_operations(FILE *fin, sensor *sensor)
+{
+
+	fread(&sensor->nr_operations, sizeof(int), 1, fin);
+	sensor->operations_idxs =
+		(int *) malloc(sensor->nr_operations * sizeof(int));
+	for (int j = 0; j < sensor->nr_operations; j++) {
+		fread(&sensor->operations_idxs[j],
+			sizeof(int), 1, fin);
+	}
+}
+
+sensor fread_PMU_sensor_values(FILE *fin)
+{
+	sensor sensor;
+
+	sensor.sensor_type = PMU;
+	sensor.sensor_data =
+		(void *) malloc(sizeof(power_management_unit));
+	power_management_unit *pmu_data =
+		(power_management_unit *)(sensor.sensor_data);
+	fread(&pmu_data->voltage, sizeof(float), 1, fin);
+	fread(&pmu_data->current, sizeof(float), 1, fin);
+	fread(&pmu_data->power_consumption, sizeof(float), 1, fin);
+	fread(&pmu_data->energy_regen, sizeof(int), 1, fin);
+	fread(&pmu_data->energy_storage, sizeof(int), 1, fin);
+
+	fread_sensor_operations(fin, &sensor);
+	return sensor;
+}
+
+
+sensor fread_tire_sensor_values(FILE *fin)
+{
+	sensor sensor;
+	sensor.sensor_type = TIRE;
+	sensor.sensor_data =
+		(void *) malloc(sizeof(tire_sensor));
+	tire_sensor *tire_data =
+		(tire_sensor *) sensor.sensor_data;
+	fread(&tire_data->pressure, sizeof(float), 1, fin);
+	fread(&tire_data->temperature, sizeof(float), 1, fin);
+	fread(&tire_data->wear_level, sizeof(int), 1, fin);
+	fread(&tire_data->performace_score, sizeof(int), 1, fin);
+
+	fread_sensor_operations(fin, &sensor);
+	return sensor;
+}
+
+void fread_sensors_array(char const *file_path,
+	int *num_sensors, sensor **sensors_array)
+{
+	// "rb" -> read binary file
+	FILE *fin = fopen(file_path, "rb");
+
+	if (fin == NULL) {
+		printf("[ERR] Something went wrong when \
+			trying to open the %s file.\n", file_path);
+		exit(EXIT_FAILURE);
+	}
+
+	// pointers to the beginning of the lists:
+	ListNode *pmu_sensors_head  = NULL;
+	ListNode *tire_sensors_head = NULL;
+
+	// pointers to the end of the lists:
+	ListNode *pmu_sensors_tail = NULL;
+	ListNode *tire_sensors_tail = NULL;
+
+	fread(num_sensors, sizeof(int), 1, fin);
+	*sensors_array = (sensor *) malloc((*num_sensors) * sizeof(sensor));
+
+	for (int i = 0 ; i < *num_sensors; i++) {
+		int tip = 0;
+		fread(&tip, sizeof(int), 1, fin);
+		if (tip == 1) {
+			// Read PMU sensor data from file
+			sensor sensor = fread_PMU_sensor_values(fin);
+
+			if (!pmu_sensors_head) {
+				// Initialize linked list
+				pmu_sensors_head = pmu_sensors_tail = new_list_node(sensor);
+			} else {
+				// Append to the end of list
+				pmu_sensors_tail->next = new_list_node(sensor);
+				pmu_sensors_tail = pmu_sensors_tail->next;
+			}
+
+		} else if (tip == 0) {
+			// Read TIRE sensor data from file
+			sensor sensor = fread_tire_sensor_values(fin);
+
+			if (!tire_sensors_head) {
+				// Initialize linked list
+				tire_sensors_head = tire_sensors_tail = new_list_node(sensor);
+			} else {
+				// Append to the end of list
+				tire_sensors_tail->next = new_list_node(sensor);
+				tire_sensors_tail = tire_sensors_tail->next;
+			}
+		}
+	}
+
+
+	// Concatenating linked lists will result in an array,
+	//     sorted by sensor priority
+	int idx = 0;
+	for (ListNode *iter = pmu_sensors_head; iter; ) {
+		ListNode *tmp = iter;
+		(*sensors_array)[idx++] = tmp->sensor;
+		iter = iter->next;
+		free(tmp);
+	}
+	for (ListNode *iter = tire_sensors_head; iter; ) {
+		ListNode *tmp = iter;
+		(*sensors_array)[idx++] = iter->sensor;
+		iter = iter->next;
+		free(tmp);
+	}
+
+	fclose(fin);
+}
+
+/*
+* Helper operationstion that I advice to be called after sorting the array
+*/
+void print_all_sensor_types(int num_sensors, sensor *sensors_array)
+{
+	for (int i = 0; i <= num_sensors - 1; i++) {
+		if (sensors_array[i].sensor_type == PMU) {
+			printf("PMU\n");
+		} else if (sensors_array[i].sensor_type == TIRE) {
+			printf("TIRE\n");
+		}
+	}
+}
+
+
+
+void command_print(int num_sensors, sensor *sensors_array)
+{
+	int idx = 0;     // sensor index
+	scanf("%d", &idx);
+	if (0 <= idx && idx <= num_sensors - 1) {
+		print_status(sensors_array[idx]);
+	} else {
+		printf("Index not in range!\n");
+	}
+
+
+}
+
+
+void command_analyze(int num_sensors, sensor *sensors_array)
+{
+
+	int idx = 0;     // sensor index
+	scanf("%d", &idx);
+	if (0 <= idx && idx <= num_sensors - 1) {
+		void *operations[NUM_OPERATIONS];
+		get_operations(operations);
+		for (int i = 0; i <
+			sensors_array[idx].nr_operations; i++) {
+			// operationstion call :
+			((void (*)()) operations[sensors_array[idx]
+				.operations_idxs[i]]) (sensors_array[idx]
+				.sensor_data);
+				// equivalent to f();
+		}
+	} else {
+		printf("Index not in range!\n");
+	}
+}
+
+
+void command_clear(int *num_sensors, sensor **sensors_array)
+{
+	int i = 0;
+	while (i < *num_sensors) {
+		if (verify_sensor((*sensors_array)[i])) {
+			// Sensor i contains erroneous data => delete it from the array
+			sensor tmp_sensor = (*sensors_array)[i];
+			for (int j = i; j <  *num_sensors - 1; j++) {
+				(*sensors_array)[j] = (*sensors_array)[j + 1];
+			}
+			(*sensors_array)[*num_sensors - 1] = tmp_sensor;
+			free((*sensors_array)[*num_sensors - 1].sensor_data);
+			free((*sensors_array)[*num_sensors - 1].operations_idxs);
+			(*num_sensors)--;
+
+			// Resize the vector after clear
+			sensor *tmp_array = (sensor *) realloc(
+				*sensors_array,
+				(*num_sensors) * sizeof(sensor)
+			);
+
+			if (tmp_array != NULL) {
+				*sensors_array = tmp_array;
+			} else {
+				// [ERR] Unable to resize sensor array
+			}
+		} else {
+			i++;
+		}
+	}
+}
+
+int main(int argc, char const *argv[])
+{
+	if (argc != 2) {
+		printf("[ERR] Invalid number of arguments.\n");
+		printf("[INFO] The program expects to receive \
+			a single argument in CLI:\n\t \
+			the path to the binary file containing sensors data.");
+		exit(EXIT_FAILURE);
+	}
+
+	int num_sensors;
+	sensor *sensors_array;
+	fread_sensors_array(argv[1], &num_sensors, &sensors_array);
+
+	char *command = (char *) malloc(COMMAND_BUFFER_LEGNTH * sizeof(char));
+	while (1) {
+		scanf("%s", command);
+		if (strstr(command, PRINT) != NULL) {
+			command_print(num_sensors, sensors_array);
+		} else if (strstr(command, ANALYSE) != NULL) {
+			command_analyze(num_sensors, sensors_array);
+		} else if (strstr(command, CLEAR) != NULL) {
+			command_clear(&num_sensors, &sensors_array);
+		} else if (strstr(command, EXIT) != NULL) {
+			break;
+		}
+	}
+
+	for (int i = 0; i < num_sensors; i++) {
+		free(sensors_array[i].sensor_data);
+		free(sensors_array[i].operations_idxs);
+	}
+	free(sensors_array);
+	free(command);
+
+	return 0;
+}
